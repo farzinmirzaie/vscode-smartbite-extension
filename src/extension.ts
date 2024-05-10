f7eff3f6411019dc35708a7c4baf111bbe585ee5
Farzin Mirzaie <fmirzaie@moneylion.com>
2024-05-10 14:19:44 +0800

    Add new command and menu item for placing an order

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,14 @@ import * as vscode from 'vscode';
 import { ActivityBarView } from './view/ActivityBarView';
 
 export const activate = (context: vscode.ExtensionContext) => {
-  // Register `ActivityBarView` as the webview provider to render the React app.
   context.subscriptions.push(
+    // Register `ActivityBarView` as the webview provider to render the React app.
     vscode.window.registerWebviewViewProvider('webview', new ActivityBarView(context)),
+
+    // Register a command to open the browser to the order page.
+    vscode.commands.registerCommand('smartbite.activityBarView.newOrder', () =>
+      vscode.env.openExternal(vscode.Uri.parse('https://canteen.trysmartbite.com/restaurants')),
+    ),
   );
 };
 
